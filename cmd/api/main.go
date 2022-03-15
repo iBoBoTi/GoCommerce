dd7fa6ce6a8a473492b61bf37fb07c530b30ed17
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-15 20:51:40 +0100

    feat: setup and run the server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
 	fmt.Println("Hello GoCommerce")
+
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 8081, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := &application{
+		config:   cfg,
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
+
+	router := gin.Default()
+	router.GET("/api-health-checker", app.healthCheckerHandler)
+
+	server := http.Server{
+		Addr:         fmt.Sprintf(":%v", cfg.port),
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	infoLog.Printf("starting %v server at port %v", cfg.env, cfg.port)
+	errorLog.Fatal(server.ListenAndServe())
+
 }
