89424bf3b25ca26d7e6a107bca698ae150a7ad8f
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-15 20:50:22 +0100

    feat: add the health check handler

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func (app *application) healthCheckerHandler(c *gin.Context) {
+	fmt.Fprintf(c.Writer, "environment: %v\n", app.config.env)
+	fmt.Fprintf(c.Writer, "api version %v\n", version)
+}
