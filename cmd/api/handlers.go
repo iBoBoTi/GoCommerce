9d141cf3b6b1dc1073278ae280a1069f3ee8dc7d
iBoBoTi <iroegbucalistus@gmail.com>
2022-03-18 09:07:03 +0100

    refactor: change the go.mod name

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,11 +1,29 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func (app *application) healthCheckerHandler(c *gin.Context) {
-	fmt.Fprintf(c.Writer, "environment: %v\n", app.config.env)
-	fmt.Fprintf(c.Writer, "api version %v\n", version)
+	reqStruct := struct {
+		Status  string `json:"status"`
+		Env     string `json:"env"`
+		Version string `json:"version"`
+	}{
+		Status:  "available",
+		Env:     app.config.env,
+		Version: version,
+	}
+	js, err := json.Marshal(reqStruct)
+	if err != nil {
+		app.errorLog.Println(err)
+		http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	js = append(js, '\n')
+	c.Writer.Header().Set("Content-Type", "application/json")
+	c.Writer.Write(js)
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -5,6 +5,7 @@ import (
 )
 
 func (app *application) Routes() *gin.Engine {
+	gin.ForceConsoleColor()
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	v1.GET("/api-health-checker", app.healthCheckerHandler)
